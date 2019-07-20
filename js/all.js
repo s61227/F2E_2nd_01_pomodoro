bb28d89ecf1da08620a59affce0dcfda0407271e
joanne <s6122712@gmail.com>
2019-07-20 18:06:44 +0800

    音樂選取播放

--- js/all.js
+++ js/all.js
@@ -1,3 +1,4 @@
+// accordin
 $(".js-accordion__list").on("click", function () {
     if ($(this).hasClass('js-accordion__active')) {
         $(this).removeClass("js-accordion__active");
@@ -10,5 +11,33 @@ $(".js-accordion__list").on("click", function () {
     }
 });
 
+// checkbox 效果
+$('.e-checkWrap').click(function(){
+    var radioStatus = $(this).find('input[type="checkbox"]').val();
+    if(radioStatus == 'on'){
+        $(this).css('text-decoration', 'line-through');
+    }else{
+        $(this).css('text-decoration', 'none');
+    }
+});
+
+// radio 效果
+var musicArry = document.querySelectorAll('.p-ringtones .e-checkWrap');
+var thisMusic = document.querySelector('audio');
+musicArry.forEach(element => {
+    element.addEventListener('click', playMusic, false);
+});
+
+function playMusic(e){
+    if(e.target.tagName === 'INPUT'){
+        if(e.target.value !== 'on'){
+            var src = 'music/'+e.target.value+'.mp3';
+            thisMusic.setAttribute('src', src);
+            thisMusic.play();
+        }else{
+            thisMusic.pause();
+        }
+    } 
+};
 
 
